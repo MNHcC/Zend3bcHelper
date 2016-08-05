5d8e941b0ac2716e2be34b28d0a49fa28acd715d
carschrotter <carschrotter@carschrotter-work>
2016-08-05 15:26:41 +0200

    refactor namespace and add composer

--- src/Module.php
+++ src/Module.php
@@ -6,16 +6,36 @@
  * and open the template in the editor.
  */
 
-namespace Zend3Helper; 
+namespace MNHcC\Zend3bcHelper;
+
+use Zend\Loader\ClassMapAutoloader;
+use Zend\Loader\StandardAutoloader;
+use Zend\ModuleManager\Feature\AutoloaderProviderInterface;
+
+const DS = \DIRECTORY_SEPARATOR;
 
 /**
  * Description of Module
  *
  * @author carschrotter
  */
-class Module {
+class Module implements AutoloaderProviderInterface {
 
-    const VERSION = '0.1.0dev';
+    const VERSION = '0.1.0-dev';
 
-    //put your code here
+    public function getAutoloaderConfig() {
+        $config = [
+            StandardAutoloader::class => [
+                'namespaces' => [
+                    __NAMESPACE__ => __DIR__ . DS . 'src' . DS . str_replace('\\', DS, __NAMESPACE__)
+                ],
+            ],
+        ];
+        //moore performance
+        if (file_exists(__DIR__ . DS . 'autoload_classmap.php')) {
+            $config[ClassMapAutoloader::class] = [__DIR__ . DS . 'autoload_classmap.php'];
+        }
+        return $config;
+    }
+
 }
