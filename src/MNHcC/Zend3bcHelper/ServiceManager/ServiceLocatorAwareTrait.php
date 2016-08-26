20f175d401e30753a075fde276616562d3169791
Michael Hegenbarth <mnh@mn-hegenbarth.de>
2016-08-26 18:20:56 +0200

    fixing use statement

--- src/MNHcC/Zend3bcHelper/ServiceManager/ServiceLocatorAwareTrait.php
+++ src/MNHcC/Zend3bcHelper/ServiceManager/ServiceLocatorAwareTrait.php
@@ -1,11 +1,21 @@
 <?php
 
 /**
- * fork from Zend framework 2.5.*
+ * MNHcC/Zend3bcHelper https://github.com/MNHcC/Zend3bcHelper
+ *
+ * @link      https://github.com/MNHcC/Zend3bcHelper for the canonical source repository
+ * @author MNHcC  - Michael Hegenbarth (carschrotter) <mnh@mn-hegenbarth.de>
+ * @copyright 2016, MNHcC  - Michael Hegenbarth (carschrotter) <mnh@mn-hegenbarth.de>
+ * @license BSD
  */
 
 namespace MNHcC\Zend3bcHelper\ServiceManager {
 
+    use Zend\ServiceManager\ServiceLocatorInterface;
+
+    /**
+     * fork from Zend framework 2.5.*
+     */
     trait ServiceLocatorAwareTrait {
 
         /**
@@ -34,4 +44,5 @@ namespace MNHcC\Zend3bcHelper\ServiceManager {
         }
 
     }
+
 }
