7385be93e8f19b5cbd63d4688b49db51fe73bd28
Michael Hegenbarth <mnh@mn-hegenbarth.de>
2017-03-25 14:35:10 +0100

    add method to get mvc modul version

--- src/Zend3bcHelper/Helper/AbstractTypeHelper.php
+++ src/Zend3bcHelper/Helper/AbstractTypeHelper.php
@@ -32,6 +32,44 @@ namespace MNHcC\Zend3bcHelper\Helper {
             return (bool) $refl->isSubclassOf(Zend3bcHelperInterface::class) || $refl->hasConstant(static::IS_ZEND_3_BC_HELPER_CLASS);
         }
 
+        /**
+         * return the mvc module minor version because in zf 3 
+         * exclude many things from default module 
+         * @staticvar int $version
+         * @return int
+         */
+        public static function wichZendMvcMajor() {
+            static $version;
+            
+            if ($version){
+                return $version;
+            }
+
+            $version = false;
+            if(!$version){
+            switch (true) {
+                /**
+                 * check is not namespace \Zend\Mvc\Router\... because zend removed router from Mvc namspace
+                 */ 
+               case (!class_exists(\Zend\Mvc\Router\RouteInterface::class) && class_exists(\Zend\Router\RouteInterface::class)): //check is v3 MVC component
+                    $version = 3;
+                    break;
+                /**
+                 * check is the new sceleton aplication (used v3 component
+                 */
+                case ( (@defined('\Application\Module::VERSION') //check for new sceleton aplication
+                && version_compare(\Application\Module::VERSION, '3.0.0dev', '>=')) ) :
+                    $version = 3;
+                    break;
+
+                default :
+                    $version = 2;
+                    break;
+            }}
+            return $version;
+        }
+        
+        
     }
 
 }
